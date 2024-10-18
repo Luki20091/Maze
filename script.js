26b62df99b34a80a29726e034bb31158dc02d38d
Luki <76406872+Luki20091@users.noreply.github.com>
2024-10-18 14:16:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,64 +4,49 @@ const cellSize = 50;
 const rows = canvas.height / cellSize;
 const cols = canvas.width / cellSize;
 let maze = [];
-let player = { x: 0, y: 0 };
+let player1 = { x: 0, y: 0 };
+let player2 = { x: cols, y: rows };
 let canDestroyWall = true;
-let score = 0;
+let p1score = 0;
+let p2score = 0;
 let fm1 = 1;
 let fm2 = 1;
 let el = 4;
 co = 0;
 
-document.getElementById('generateMaze').addEventListener('click', generateMaze);
-document.addEventListener('keydown', movePlayer);
+document.addEventListener('keydown', moveplayer1);
+document.addEventListener('keydown', moveplayer2);
 
 updateScore();
 
 function generateMaze() {
     maze = Array.from({ length: rows }, () => Array(cols).fill(0));
     
-    maze[1][1] = 0;
-    maze[0][1] = 0;
-    maze[1][0] = 0;
-    
-    maze[0][0] = 0; // Start point
-
-    fm1 = (Math.floor(Math.random() * 5) + 1);
-    fm2 = (Math.floor(Math.random() * 5) + 1);
-
 
     for (let y = 0; y < rows; y++) {
         for (let x = 0; x < cols; x++) {
-            maze[y][x] = Math.random() > 0.9 ? 1 : 0; // 1 walls
+            maze[y][x] = Math.random() > 0.9 ? 1 : ( Math.random() > 0.93 ? 2 : 0); // 1 walls, 2 points
         }
     }
 
+    maze[1][1] = 0;
+    maze[0][1] = 0;
+    maze[1][0] = 0;
+    maze[0][0] = 0; // Start point1
+    
+    maze[rows-2][cols-2] = 0;
+    maze[rows-1][cols-2] = 0;
+    maze[rows-2][cols-1] = 0;
+    maze[rows-1][cols-1] = 0; // Start point2
 
 
-    maze[rows - fm2][cols - fm1] = 0;
-
-    player = { x: 0, y: 0 };
+    player1 = { x: 0, y: 0 };
+    player2 = { x: cols-1, y: rows-1 };
     updateScore();
     drawMaze();
 }
 
 function drawMaze() {
-    el++;
-    if (el > 3 ) {
-        el = 0;
-        for (let y = 0; y < rows; y++) {
-            for (let x = 0; x < cols; x++) {
-                if (y != player.y && x != player.x) {
-                    if (maze[y][x] === 0 || maze[y][x] === 2) {
-                        maze[y][x] = Math.random() > 0.7 ? 2 : 0; //2 reds
-                        co++;
-                    }
-                }
-            }
-        }
-    }
-
-
     co = 0;
 
     
@@ -74,7 +59,8 @@ function drawMaze() {
     }
 
     if (co <= 0) {
-        maze[rows - fm2][cols - fm1] = 4;
+        alert("WON");
+        generateMaze();
     }
 
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -85,7 +71,7 @@ function drawMaze() {
                 ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
             }
             if (maze[y][x] === 2) {
-                ctx.fillStyle = '#f00';
+                ctx.fillStyle = '#0b0';
                 ctx.beginPath();
                 ctx.arc(x * cellSize + cellSize / 2, y * cellSize + cellSize / 2, cellSize / 2, 0, Math.PI * 2);
                 ctx.fill();
@@ -98,41 +84,85 @@ function drawMaze() {
                 ctx.fillStyle = '#0f0';
                 ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
             }
+            if (maze[y][x] === 5) {
+                ctx.fillStyle = '#fbb';
+                ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
+            }
         }
     }
     ctx.fillStyle = '#66f';
-    ctx.fillRect(player.x * cellSize, player.y * cellSize, cellSize, cellSize);
+    ctx.fillRect(player1.x * cellSize, player1.y * cellSize, cellSize, cellSize);
+
+    ctx.fillStyle = '#f66';
+    ctx.fillRect(player2.x * cellSize, player2.y * cellSize, cellSize, cellSize);
     
 }
 
-function movePlayer(e) {
-    const { x, y } = player;
-    if (e.key === 'w' && y > 0 && ((maze[y - 1][x] !== 1))){ maze[y][x] = 3; player.y--};
-    if (e.key === 's' && y < rows - 1 && ((maze[y + 1][x] !== 1))){ maze[y][x] = 3; player.y++};
-    if (e.key === 'a' && x > 0 && ((maze[y][x - 1] !== 1))){ maze[y][x] = 3; player.x--};
-    if (e.key === 'd' && x < cols - 1 && ((maze[y][x + 1] !== 1))){ maze[y][x] = 3;  player.x++};
-    if (e.key === ' ' && canDestroyWall) destroyWallsAroundPlayer();
+function moveplayer1(e) {
+    const { x, y } = player1;
+    if (e.key === 'w' && y > 0 && ((maze[y - 1][x] !== 1) && (maze[y - 1][x] !== 5))){ maze[y][x] = 3; player1.y--};
+    if (e.key === 's' && y < rows - 1 && ((maze[y + 1][x] !== 1) && (maze[y + 1][x] !== 5))){ maze[y][x] = 3; player1.y++};
+    if (e.key === 'a' && x > 0 && ((maze[y][x - 1] !== 1) && (maze[y][x - 1] !== 5))){ maze[y][x] = 3; player1.x--};
+    if (e.key === 'd' && x < cols - 1 && ((maze[y][x + 1] !== 1) && (maze[y][x + 1] !== 5))){ maze[y][x] = 3;  player1.x++};
+    if (maze[player1.y][player1.x] === 2) {
+        maze[player1.y][player1.x] = 3;
+        saveScore(1);
+        aroundPlayer1();
+    }
+    if (maze[player1.y][player1.x] === 0) {
+        maze[player1.y][player1.x] = 3;
+        saveScore(1);
+    }
     drawMaze();
-    if (maze[player.y][player.x] === 4) {
-        generateMaze();
-    }
-    if (maze[player.y][player.x] === 2) {
-        saveScore();
-    }
 }
+
+function moveplayer2(e) {
+    const { x, y } = player2;
+    if (e.key === 'i' && y > 0 && ((maze[y - 1][x] !== 1) && (maze[y - 1][x] !== 3))){ player2.y--};
+    if (e.key === 'k' && y < rows - 1 && ((maze[y + 1][x] !== 1) && (maze[y + 1][x] !== 3))){ player2.y++};
+    if (e.key === 'j' && x > 0 && ((maze[y][x - 1] !== 1) && (maze[y][x - 1] !== 3))){ player2.x--};
+    if (e.key === 'l' && x < cols - 1 && ((maze[y][x + 1] !== 1) && (maze[y][x + 1] !== 3))){ player2.x++};
+    if (maze[player2.y][player2.x] === 2) {
+        maze[player2.y][player2.x] = 5;
+        saveScore(2);
+        aroundPlayer2();
+    }
+    if (maze[player2.y][player2.x] === 0) {
+        maze[player2.y][player2.x] = 5;
+        saveScore(2);
+    }
+    drawMaze();
+}
+
 
 generateMaze();
 
 
 
+function updateScore() {
+    document.getElementById('p1-score').innerText = `Wynik: ` + p1score;
+    document.getElementById('p2-score').innerText = `Wynik: ` + p2score;
+}
+
+function saveScore(p) {
+    if (p === 1) p1score++;
+    if (p === 2) p2score++;
+
+    updateScore();
+}
 
 
 
 
 
 
-function destroyWallsAroundPlayer() {
-    const { x, y } = player;
+
+
+
+
+
+function aroundPlayer1() {
+    const { x, y } = player1;
     const directions = [
         { x: x + 1, y },
         { x: x - 1, y },
@@ -141,25 +171,28 @@ function destroyWallsAroundPlayer() {
     ];
 
     directions.forEach(({ x: nx, y: ny }) => {
-        if (nx >= 0 && ny >= 0 && nx < cols && ny < rows && maze[ny][nx] === 1) {
-            maze[ny][nx] = 0; // Change wall to passable
+        if (nx >= 0 && ny >= 0 && nx < cols && ny < rows) {
+            maze[ny][nx] = 3; // Change like as boost
         }
     });
-
-    canDestroyWall = false;
-    document.getElementById('time').innerText = 'Bomba: 5s';
-   
-    setTimeout(() => {
-        document.getElementById('time').innerText = 'Bomba: [Spacja]';
-        canDestroyWall = true;
-    }, 5000);
 }
 
-function updateScore() {
-    document.getElementById('score').innerText = `Wynik: ` + score;
-}
 
-function saveScore() {
-    score++;
-    updateScore();
+
+
+
+function aroundPlayer2() {
+    const { x, y } = player2;
+    const directions = [
+        { x: x + 1, y },
+        { x: x - 1, y },
+        { x, y: y + 1 },
+        { x, y: y - 1 }
+    ];
+
+    directions.forEach(({ x: nx, y: ny }) => {
+        if (nx >= 0 && ny >= 0 && nx < cols && ny < rows) {
+            maze[ny][nx] = 5; // Change like as boost
+        }
+    });
 }
